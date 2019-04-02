b6ed2d44b4ba9fe7c0d5b30850964e42fb3307df
aloha66 <lzcbruce@163.com>
2019-04-02 21:08:02 +0800

    [新增]查询单个post,更新post [新增]分页对象 [优化]把user_id存到token里面

--- service/post.js
+++ service/post.js
@@ -1,23 +1,50 @@
 const mongoose = require("mongoose");
 
-const add = async (ctx,next) => {
-    const {title,content,user_id} = ctx.request.body
-    const Post = mongoose.model("Post");
-    const post_id = (await Post.countDocuments()) + 1;
-    const post = new Post({ title, content, user_id,post_id });
-    post.save(); 
-}
+const add = async (ctx, next) => {
+  const { user_id } = ctx.state.user.data;
+  const { title, content } = ctx.request.body;
+  const Post = mongoose.model("Post");
+  const post_id = (await Post.countDocuments()) + 1;
+  const post = new Post({ title, content, user_id, post_id });
+  await post.save();
+};
 
-const fetch = async (ctx,next) => {
-    const {title,content,user_id} = ctx.request.body
-    const Post = mongoose.model("Post");
-    return await Post.find();
-}
+const fetch = async (ctx, next) => {
+  const { user_id } = ctx.state.user.data;
+  const { offset, title } = ctx.query;
+  const Post = mongoose.model("Post");
+  let total = await Post.countDocuments();
+  const data = await Post.find({ user_id, title: new RegExp(title) }, null, {
+    skip: parseInt(offset)
+  }).limit(10);
+  if (title) total = data.length;
+  return { data, pagation: { total } };
+  //   return await Post.find({ user_id }).limit(parseInt(size));
+};
 
-const del = async (ctx,next) => {
-    const {post_id} = ctx.params
-    const Post = mongoose.model("Post");
-    return await Post.remove({post_id});
-}
+const fetchOne = async (ctx, next) => {
+  const { user_id } = ctx.state.user.data;
+  const { post_id } = ctx.params;
+  const Post = mongoose.model("Post");
+  return await Post.findOne({ user_id, post_id });
+};
 
-module.exports = { add,fetch,del};
+const del = async (ctx, next) => {
+  const { user_id } = ctx.state.user.data;
+  const { post_id } = ctx.params;
+  const Post = mongoose.model("Post");
+  return await Post.deleteOne({ post_id, user_id });
+};
+
+const update = async (ctx, next) => {
+  const { user_id } = ctx.state.user.data;
+  const { post_id } = ctx.params;
+  const { title, content } = ctx.request.body;
+  const Post = mongoose.model("Post");
+  return await Post.update(
+    { post_id, user_id },
+    { title, content, update_at: new Date() }
+  );
+};
+
+module.exports = { add, fetch, del, fetchOne, update };
